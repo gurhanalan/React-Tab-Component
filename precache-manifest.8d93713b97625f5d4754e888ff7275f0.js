2f29c40ed69ee1e753145263ce3f396d887a95ba
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-05 14:00:39 +0300

    Updates

---
+++ precache-manifest.8d93713b97625f5d4754e888ff7275f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418efe50f98d9e18fdd21b9835bf090b",
+    "url": "/React-Tab-Component/index.html"
+  },
+  {
+    "revision": "cc4d5745486d0afe0b8d",
+    "url": "/React-Tab-Component/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "60aba34b0321597c0352",
+    "url": "/React-Tab-Component/static/js/2.ed232c55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tab-Component/static/js/2.ed232c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4d5745486d0afe0b8d",
+    "url": "/React-Tab-Component/static/js/main.7818e9b4.chunk.js"
+  },
+  {
+    "revision": "c863494f316e3f3ca859",
+    "url": "/React-Tab-Component/static/js/runtime-main.9639680e.js"
+  }
+]);
